5e8087fbc9317956415b14a05c514a45dc39133b
Julie Brun Tørngren Nielsen <juli839d@VID.net.local>
2019-09-12 12:47:29 +0200

    made deleteQuotes funktion

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import AddQuotes from './components/AddQuotes';
 class App extends Component {
   state = {
     quotes:[
-      {quoteTitle: 'To know, or...', quoteContent: 'The only true wisdom is in knowing, you know nothing', author: 'Socrates', id: 1},
-      {quoteTitle: 'A lovely country, it is', quoteContent: 'God bless Denmark', author: 'Queen Margrethe II', id: 2},
-      {quoteTitle: 'Keep on dreaming', quoteContent: 'You may say i´m a dreamer, but im not the only one', author: 'John Lennon', id: 3},
-      {quoteTitle: 'May the clouds be as soft as his mattresses', quoteContent: 'Goddag jeg hedder Lars Larsen...', author: 'Lars Larsen', id: 4},
-      {quoteTitle: 'Imagination!', quoteContent: 'Squidward, we don´t need a television. Not as long as we have our...', author: 'Spongebob', id: 5}
+      {title: 'To know, or...', content: 'The only true wisdom is in knowing, you know nothing', author: 'Socrates', id: 1},
+      {title: 'A lovely country, it is', content: 'God bless Denmark', author: 'Queen Margrethe II', id: 2},
+      {title: 'Keep on dreaming', content: 'You may say i´m a dreamer, but im not the only one', author: 'John Lennon', id: 3},
+      {title: 'May the clouds be as soft as his mattresses', content: 'Goddag jeg hedder Lars Larsen...', author: 'Lars Larsen', id: 4},
+      {title: 'Imagination!', content: 'Squidward, we don´t need a television. Not as long as we have our...', author: 'Spongebob', id: 5}
     ]
+  }
+  deleteQuotes = (id) => {
+    let quotes = this.state.quotes.filter(quote => {
+      return quote.id !== id
+    });
+    this.setState({
+      quotes: quotes
+    })
   }
   render() {
     return (
@@ -19,7 +27,7 @@ class App extends Component {
           <h1>Quotes</h1>
         </header>
         <main className="container">
-          <Quotes quotes={this.state.quotes}/>
+          <Quotes quotes={this.state.quotes} deleteQuotes={this.deleteQuotes}/>
         </main>
       </div>
     );
--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,15 +1,20 @@
 import React from 'react'
 
-const Quotes = ({quotes}) => {
-    const quoteList = quotes.map(quote => {
-        return (
-            <div className="quote-card" key="quote.id">
-                <h3>{ quote.quoteTitle }</h3>
-                <p>{ quote.quoteContent }</p>
-                <p>{ quote.author}</p>
-            </div>
-        )
-    })
+const Quotes = ({quotes, deleteQuotes}) => {
+    const quoteList = quotes.length ? (
+        quotes.map(quote => {
+            return (
+                <div className="quote-card" key={ quote.id }>
+                    <h3>{ quote.title }</h3>
+                    <p>{ quote.content }</p>
+                    <p>{ quote.author }</p>
+                    <button onClick={() => {deleteQuotes(quote.id)}}>Delete Quote</button>
+                </div>
+            )
+        })
+    ) : (
+        <p>There are no quotes left</p>
+    )
     return (
         <div className="quote-list">
             {quoteList}
